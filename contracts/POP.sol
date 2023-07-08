17a23bc51107f5d49de0bcc95ccea4d3c2c314c2
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-07-08 12:37:26 +0100

    contractURI changes + fix import

--- contracts/POP.sol
+++ contracts/POP.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import "@openzeppelin/contracts/token/ERC1155";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -15,14 +15,14 @@ contract POP is ERC1155, ERC1155Burnable, Ownable {
     uint256 public constant POP_GOLD = 2;
 
     string public baseURI;
-    string public contractURI;
+    string public collectionContractURI;
 
     event BaseURIChanged(address sender, string newBaseURI);
     event ContractURIChanged(address sender, string newContractURI);
 
     constructor(string memory _baseURI, string memory _contractURI) ERC1155(_contractURI) {
         baseURI = _baseURI;
-        contractURI = _contractURI;
+        collectionContractURI = _contractURI;
 
         _mint(msg.sender, POP_BRONZE, 100, "");
         _mint(msg.sender, POP_SILVER, 25, "");
@@ -43,15 +43,19 @@ contract POP is ERC1155, ERC1155Burnable, Ownable {
         uint256[] memory amounts,
         bytes memory data
     ) internal override {
-        super._beforeTokenTransfer(operator, from, to, ids, amount, data);
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
         require(
-            msg.sender == owner() || to == address(),
+            msg.sender == owner() || to == address(0),
             "Token cannot be transferred, only burned"
         );
     }
 
-    function uri(uint256 _tokenId) public pure override returns (string memory) {
-        return string(abi.encodePacked(contractURI, _tokenId.toString()));
+    function contractURI() public view returns (string memory) {
+        return collectionContractURI;
+    }
+
+    function uri(uint256 _tokenId) public view override returns (string memory) {
+        return string(abi.encodePacked(baseURI, _tokenId.toString()));
     }
 
     function setBaseURI(string calldata _newBaseURI) public onlyOwner {
@@ -60,7 +64,7 @@ contract POP is ERC1155, ERC1155Burnable, Ownable {
     }
 
     function setContractURI(string calldata _newContractURI) public onlyOwner {
-        contractURI = _newContractURI;
+        collectionContractURI = _newContractURI;
         emit ContractURIChanged(msg.sender, _newContractURI);
     }
 }
