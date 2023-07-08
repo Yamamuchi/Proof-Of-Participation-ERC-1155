ae91907ca1f91661fae4219ce9e9e54001d6b955
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-07-08 12:10:47 +0100

    deploy contract + verify on sepolia.etherscan

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,23 @@
 const hre = require("hardhat");
 
 async function main() {
+  const baseURI = "ipfs://QmcyEvXcautKftdeVrYhHLjRrHKbyR4q7cx4vtrouPnzBa/";
+  const contractURI = "ipfs://QmXFpCe9JxFP6te6E79Su2wbqZ5YSVPKbhorKfjXX3M9YD";
+
+  const POP = await hre.ethers.getContractFactory("POP");
+  const POPDeployed = await POP.deploy(baseURI, contractURI);
+
+  await POPDeployed.deployed();
+
+  console.log("Deployed POP to: ", POPDeployed.address);
+
+  await hre.run("verify:verify", {
+    address: POPDeployed.address,
+    constructorArguments: [
+      baseURI,
+      contractURI
+    ]
+  });
 }
 
 main().catch((error) => {
